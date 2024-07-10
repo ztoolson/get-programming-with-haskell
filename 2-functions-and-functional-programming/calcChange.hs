439f0a4cced6823a1ff343a965b7c5df1e456736
Zach Toolson <zach@mercury.com>
2024-07-10 16:20:53 -0600

    Update calcChange version to write a function using where statement

--- 2-functions-and-functional-programming/calcChange.hs
+++ 2-functions-and-functional-programming/calcChange.hs
@@ -1,5 +1,15 @@
 main = print (calcChange 5 20)
 
-calcChange owed given = if given - owed > 0
-    then given - owed
+calcChange owed given = if change > 0
+    then change
     else 0
+    where change = given - owed
+
+-- exercises
+
+inc x = x + 1
+double x = x * 2
+square x = x * x
+
+f n = if isEven then n - 2 else 3 * n + 1
+    where isEven = even n
